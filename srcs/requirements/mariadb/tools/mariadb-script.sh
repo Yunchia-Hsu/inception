5f42aa1597ebdb98e49727d2f85ab287583a7880
yun-chiahsu <yh24hsu@gmail.com>
2025-04-14 12:01:33 +0300

    added dickerfiles

---
+++ srcs/requirements/mariadb/tools/mariadb-script.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+echo "You are setting up MariaDB directory..."
+chmod -R 755 /var/lib/mysql
+
+
+mkdir -p /run/mysql
+chown -R myaql:mysql /var/lib/mysql /run/mysqld
+#check if mariadb is init
+if [! -d "/var/lib/mysql/mysql"]; then
+    echo "Initializing MariaDB system tables..."
+    mariadb-install-db --basedir=/usr --user=mysql --datadir=/var/lib/mysql #>/dev/null
+
+    echo "setting yo wordpress database and user~"
+    mysqld --user=mysql --bootstrap << EOF
+
+USE mysql;
+FLUSH PRIVILEGES;
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD";
+CREATE DATABASE $WORDPRESS_DATABASE_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER $WORDPRESS_DATABASE_USER@'%' IDENTIFIED BY "$WORDPRESS_DATABASE_PASSWORD";
+GRANT ALL PRIVILETES ON $WORDPRESS_DATABASE_NAME.* TO $WORDPRESS_DATABASE_USER@'%';
+FLUSG PRIVILEGES;
+
+else
+    echo "MariaDB  was initialized. Database and users were confihured."
+fi
+
+echo "mariadb server is running..."
+exec mysqld --defaults-file=/etc/my.cnf.d/mariadb_config
