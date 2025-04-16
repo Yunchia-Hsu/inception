c4ffc9e119f85da20f28c9bd338ed5fa1a1cceb4
Yunchia-Hsu <yh42hsu@gmail.com>
2025-04-16 15:41:15 +0300

    makefile done 2

--- srcs/requirements/mariadb/tools/mariadb-script.sh
+++ srcs/requirements/mariadb/tools/mariadb-script.sh
@@ -3,16 +3,15 @@ echo "You are setting up MariaDB directory..."
 chmod -R 755 /var/lib/mysql
 
 
-mkdir -p /run/mysql
+mkdir -p /run/mysqld
 chown -R mysql:mysql /var/lib/mysql /run/mysqld
 #check if mariadb is init
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing MariaDB system tables..."
-    mariadb-install-db --basedir=/usr --user=mysql --datadir=/var/lib/mysql #>/dev/null
+    mariadb-install-db --basedir=/usr --user=mysql --datadir=/var/lib/mysql >/dev/null
 
     echo "setting up wordpress database and user~"
     mysqld --user=mysql --bootstrap << EOF
-
 USE mysql;
 FLUSH PRIVILEGES;
 
@@ -21,6 +20,7 @@ CREATE DATABASE $WORDPRESS_DATABASE_NAME CHARACTER SET utf8 COLLATE utf8_general
 CREATE USER $WORDPRESS_DATABASE_USER@'%' IDENTIFIED BY "$WORDPRESS_DATABASE_PASSWORD";
 GRANT ALL PRIVILETES ON $WORDPRESS_DATABASE_NAME.* TO $WORDPRESS_DATABASE_USER@'%';
 FLUSG PRIVILEGES;
+EOF
 
 else
     echo "MariaDB  was initialized. Database and users were confihured."
