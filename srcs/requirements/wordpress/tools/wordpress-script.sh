c39b88541f326af2b1ab075d47cac863ee2de4f0
Yunchia-Hsu <yh42hsu@gmail.com>
2025-04-17 17:21:31 +0300

    added script

--- srcs/requirements/wordpress/tools/wordpress-script.sh
+++ srcs/requirements/wordpress/tools/wordpress-script.sh
@@ -11,7 +11,7 @@ wget -q https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x /usr/local/bin/wp
 
 echo "check if mariadb is running before running wordpress"
-# ping  check if mariadb can be connected 
+
 mariadb-admin ping --protocol=tcp --host=mariadb -u $WORDPRESS_DATABASE_USER --password=$WORDPRESS_DATABASE_USER_PASSWORD --wait=300 
 
 if [ ! -f /var/www/html/wp-config.php ]; then
