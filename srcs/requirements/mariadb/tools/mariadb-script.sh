1876d600610c1316211e9e13d415ac7276df7447
Yunchia-Hsu <yh42hsu@gmail.com>
2025-04-17 13:42:09 +0300

    change mariadb user permissionn

--- srcs/requirements/mariadb/tools/mariadb-script.sh
+++ srcs/requirements/mariadb/tools/mariadb-script.sh
@@ -1,39 +1,31 @@
 #!/bin/sh
-echo "You are setting up MariaDB directory..."
+
+echo "==> Setting up MariaDB directory..."
 chmod -R 755 /var/lib/mysql
 
+mkdir -p /run/mysqld 
 
-mkdir -p /run/mysqld
 chown -R mysql:mysql /var/lib/mysql /run/mysqld
-#check if mariadb is init
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Initializing MariaDB system tables..."
+    echo "==> Initializing MariaDB system tables..."
     mariadb-install-db --basedir=/usr --user=mysql --datadir=/var/lib/mysql >/dev/null
 
-    echo "setting up wordpress database and user~"
+    echo "==> Creating WordPress database and user..."
     mysqld --user=mysql --bootstrap << EOF
 USE mysql;
 FLUSH PRIVILEGES;
 
-
-
+ALTER USER 'root'@'localhost' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD";
 CREATE DATABASE $WORDPRESS_DATABASE_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-CREATE USER 'yhsu'@'localhost' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD";
-GRANT ALL PRIVILEGES ON $WORDPRESS_DATABASE_NAME.* TO 'yhsu'@'localhost';
-
-
 CREATE USER $WORDPRESS_DATABASE_USER@'%' IDENTIFIED BY "$WORDPRESS_DATABASE_PASSWORD";
 GRANT ALL PRIVILEGES ON $WORDPRESS_DATABASE_NAME.* TO $WORDPRESS_DATABASE_USER@'%';
-
-
 FLUSH PRIVILEGES;
-
 EOF
 
 else
-    echo "MariaDB  was initialized. Database and users were configured."
+    echo "==> MariaDB is already installed. Database and users are configured."
 fi
 
-echo "mariadb server is running..."
+echo "==> Starting MariaDB server..."
 exec mysqld --defaults-file=/etc/my.cnf.d/mariadb_config
